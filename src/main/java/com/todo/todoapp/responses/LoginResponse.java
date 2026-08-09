d68712ffe5dabb335ddd2c328b2d87473bb77e88
Dorian Mayamba <dorian.mayamba@gmail.com>
2026-08-09 11:25:42 +0100

    Added json properties for Response classes

--- src/main/java/com/todo/todoapp/responses/LoginResponse.java
+++ src/main/java/com/todo/todoapp/responses/LoginResponse.java
@@ -1,8 +1,13 @@
 package com.todo.todoapp.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record LoginResponse(
+        @JsonProperty("token")
         String accessToken,
+        @JsonProperty("type")
         String tokenType,
+        @JsonProperty("user")
         UserSummary userSummary
 ) {
 }
--- src/main/java/com/todo/todoapp/responses/UserSummary.java
+++ src/main/java/com/todo/todoapp/responses/UserSummary.java
@@ -1,5 +1,6 @@
 package com.todo.todoapp.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.todo.todoapp.details.UserInfoDetails;
 import com.todo.todoapp.models.Role;
 import org.springframework.security.core.GrantedAuthority;
@@ -8,9 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public record UserSummary(
+        @JsonProperty("id")
         int userId,
+        @JsonProperty("firstName")
         String firstName,
+        @JsonProperty("email")
         String username,
+        @JsonProperty("roles")
         List<String> roles
 ) {
     public UserSummary(UserInfoDetails userInfoDetails) {
